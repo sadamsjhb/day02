7aba15b9a489bef641efca211c8251977d331d85
shelton Adams <sadams@c4r9s3.wethinkcode.co.za>
2020-06-19 09:24:25 +0200

    exercise 02

--- ex02/ft_print_numbers.c
+++ ex02/ft_print_numbers.c
@@ -1,15 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sadams <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/19 09:23:35 by sadams            #+#    #+#             */
+/*   Updated: 2020/06/19 09:24:02 by sadams           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
 void ft_putchar(int c);
 
 void ft_print_numbers(void)
 {
-int digit;
+	int digit;
 digit = '0';
 
 while(digit <= '9')
 {
-ft_putchar(digit);
-digit++;
+	ft_putchar(digit);
+	digit++;
 }
 }
