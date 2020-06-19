eb07ceb840137145b02ca54d3190d6202369a68d
shelton Adams <sadams@c4r9s3.wethinkcode.co.za>
2020-06-19 09:22:18 +0200

    exercise 01

--- ex01/ft_print_reverse_alphabet.c
+++ ex01/ft_print_reverse_alphabet.c
@@ -1,15 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sadams <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/19 09:20:58 by sadams            #+#    #+#             */
+/*   Updated: 2020/06/19 09:21:38 by sadams           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
 void ft_putchar(char c);
 
 void ft_print_reverse_alphabet(void)
 {
-char alphabet;
+	char alphabet;
 
 alphabet = 'z';
 while(alphabet >= 'a')
 {
-ft_putchar(alphabet);
-alphabet--;
+	ft_putchar(alphabet);
+	alphabet--;
 }
 }
