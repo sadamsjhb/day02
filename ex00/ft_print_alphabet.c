fee4d8092c3244a3bcf60f39c051bebd9f8656c1
shelton Adams <sadams@c4r9s3.wethinkcode.co.za>
2020-06-19 09:19:51 +0200

    exercise 00

--- ex00/ft_print_alphabet.c
+++ ex00/ft_print_alphabet.c
@@ -1,15 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sadams <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/19 09:17:52 by sadams            #+#    #+#             */
+/*   Updated: 2020/06/19 09:18:59 by sadams           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
 void ft_putchar(char c);
 
 void ft_print_alphabet(void)
 {
-char alphabet;
+	char alphabet;
 
 alphabet = 'a';
 while (alphabet <= 'z')
 {
-ft_putchar(alphabet);
-alphabet++;
+	ft_putchar(alphabet);
+	alphabet++; 
 }
 }
