dffa58f6e607cb813603fcf34e16da0200f0b267
shelton Adams <sadams@c4r9s3.wethinkcode.co.za>
2020-06-20 08:53:54 +0200

    exercise 05

--- ex05/ft_print_comb2.c
+++ ex05/ft_print_comb2.c
@@ -1,32 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sadams <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/20 08:48:24 by sadams            #+#    #+#             */
+/*   Updated: 2020/06/20 08:53:20 by sadams           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
 int ft_putchar(char c)
 {
-write(1, &c, 1);
-return (0);
+	write(1, &c, 1);
+	return (0);
 }
 
 void ft_print_comb2(void)
 {
-int a;
-int b;
+	int a;
+	int b;
 
-a = 0;
-while(++a <= 99)
-{
-b = a + 1;
-while(++b <= 99)
-{
-ft_putchar(a / 10 + '0');
-ft_putchar(a % 10 + '0');
-ft_putchar(' ');
-ft_putchar(b / 10 + '0');
-ft_putchar(b % 10 + '0');
-if((a != 98 || b != 99))
-{
-ft_putchar(',');
-ft_putchar(' ');
-}
-}
-}
+	a = 0;
+	while(++a <= 99)
+	{
+		b = a + 1;
+		while(++b <= 99)
+		{
+			ft_putchar(a / 10 + '0');
+			ft_putchar(a % 10 + '0');
+			ft_putchar(' ');
+			ft_putchar(b / 10 + '0');
+			ft_putchar(b % 10 + '0');
+			if((a != 98 || b != 99))
+			{
+				ft_putchar(',');
+				ft_putchar(' ');
+			}
+		}
+	}
 }
